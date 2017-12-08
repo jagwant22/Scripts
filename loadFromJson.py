de70fd18d26fd99e1d58d3df261022c0ed877193
Jagwant Sehgal <jagwantsehgal@Jagwants-MacBook-Air.local>
2017-12-08 09:30:55 +0530

    updated the script

--- loadFromJson.py
+++ loadFromJson.py
@@ -4,6 +4,13 @@ import json
 from django.contrib.auth.models import User
 from navia_apis.models import UserProfile, Doctor
 
+linkWith = "doctor_number"
+
+uhid_dictionary = {
+	"uhid":"",
+	"doctor_number" : linkWith,
+	"partner" : "INDIVIDUAL"
+}
 
 jsonData = []
 with open('doctor_database.json') as jsonFile:
@@ -25,10 +32,55 @@ for patient in jsonData:
 		print patient
 		print "error info : \n"
 		print str(Err)
-	
+
+	# Add UHID
+	uhid_dictionary["uhid"] = patient['patient_number'] 
+
+	# Handle DOB
+	dob = cleanDOB(patient["dob"])
+
 	# Create UserProfile
 	user_profile_object = UserProfile(
-			
+			user = user_object,
+			linked_doctors = json.dumps([]),
+			uhids = json.dumps([uhid_dictionary]),
+			device_id = 0007,
+			gender = patient['gender'],
+			dob = dob
+
 		)
 
+	user_profile_object.save()
+
 	# Link Patient to Doctor
+
+	try:
+		doc_object = Doctor.objects.get(phone = linkWith)
+		linked_members = json.loads(doc_object.linked_patients)
+		linked_members.append(patient['mobile_number'])
+		doc_object.linked_patients = json.dumps(linked_members)
+		doc_object.save()
+	except Exception as Error:
+		print "Error linking patients ::: to doctor"
+		print Error
+		print "Error in patient :",
+		print patient
+
+
+
+
+def cleanDOB(dob):
+	try:
+		split_dob = dob.split("-")
+		returnDob = ""
+		if len(split_dob[0]) == 4 :
+			returnDob = split_dob[0] + "-" + split_dob[1] + "-" + split_dob[2]
+
+		else :
+			returnDob = split_dob[2] + "-" + split_dob[1] + "-" + split_dob[0]
+	except:
+		returnDob = "1990-01-01"
+
+	return returnDob
+
+
